4f70f653c7c384ec978cd4622e64ea6b37d787da
Camila Marin Aguirre <camila.x.marin.aguire.-nd@disney.com>
2023-03-16 21:46:48 -0500

    adding skills

--- personal-portfolio/src/Skills.jsx
+++ personal-portfolio/src/Skills.jsx
@@ -1,41 +1,14 @@
 import React from 'react'
 
-export default function Skills({ skill }) {
+export default function Skills({ skills }) {
 
 	return (
 		<div>
 			<p>I love learning new things. Here is a list of technologies I have worked with.</p>
 			<div className='skills-grid'>
-				<div className='skill-box'>
-					<p>HTML5</p>
-				</div>
-				<div className='skill-box'>
-					<p>CSS3</p>
-				</div>
-				<div className='skill-box'>
-					<p>Javascript</p>
-				</div>
-				<div className='skill-box'>
-					<p>React</p>
-				</div>
-				<div className='skill-box'>
-					<p>Angular+9</p>
-				</div>
-				<div className='skill-box'>
-					<p>Typescript</p>
-				</div>
-				<div className='skill-box'>
-					<p>Jasmine</p>
-				</div>
-				<div className='skill-box'>
-					<p>Jest</p>
-				</div>
-				<div className='skill-box'>
-					<p>GraphQl</p>
-				</div>
-				<div className='skill-box'>
-					<p>Github</p>
-				</div>
+				{skills.map((skill) =>
+					<p className='skill-box' key={skill}>{skill}</p> // key to identify each item in the list
+				)}
 			</div>
 		</div>
 	)
